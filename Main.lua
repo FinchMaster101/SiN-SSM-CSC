ea15bf4b753c00b62f1690ac6948111e438da8cb
FinchMaster101 <68505550+FinchMaster101@users.noreply.github.com>
2020-09-03 21:16:20 +0200

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -86,21 +86,11 @@ end;
 if not OldPatchedSP then OldPatchedSP = SinglePlayer.Client.OnUpdate end
 
 SinglePlayer.Client.OnUpdate = function(self, dt)
-
 	OldPatchedSP(self, dt)
-
 	if not (g_gameRules.class == "InstantAction" or g_gameRules.class == "PowerStruggle") then
-
 		return;
-
 	end
-	
-	if(LAST_UPDATE)then LAST_UPDATE = tonumber(LAST_UPDATE); end;
-	LAST_UPDATE = LAST_UPDATE or _time - SIN_AI_UPDATE_DELAY;
-	
-	if(_time - LAST_UPDATE >= SIN_AI_UPDATE_DELAY)then
-		if(UPDATE_AI_ENTITIES)then
-		LAST_UPDATE = _time;
+	if(UPDATE_AI_ENTITIES)then
 		local allScouts=System.GetEntitiesByClass("Scout");
 		local allGrunts=System.GetEntitiesByClass("Grunt");
 		local allAIEntities = {};
@@ -113,50 +103,14 @@ SinglePlayer.Client.OnUpdate = function(self, dt)
 		end;
 		local updated=0
 		for i,v in ipairs(allAIEntities or {}) do
-			--if(v.AI and v.AI.lastHitTarget)then
-			--	if(v.lastHitTime and (_time - v.lastHitTime < 25))then
-			--		local aimPos = System.GetEntity(v.AI.lastHitTarget);
-			--		if(aimPos and aimPos.actor and aimPos.actor:GetHealth()>0)then
-
-						v:SetDirectionVector(TryGetDir(v)); -- mike cause strange directions if SIN_AI_UPDATE_DELAY isn't low enough.
-
-						--if(v.hit_dir)then v:SetDirectionVector(GNV(v.hit_dir)) end; -- didnt work too :s
-						--v:SetDirectionVector(GNV(GetDirectionVector(v:GetPos(), aimPos:GetPos(), true))); -- does weird bugs after some time :s
-						updated=updated+1;
-			--		else
-			--			if(SIN_LOG_VERBOSITY and SIN_LOG_VERBOSITY>2)then
-			--				printf("$9[$4SiN$9] AISystem: AimTarget not found for " .. v:GetName())
-			--			end;
-			--		end;
-			--	end;
-			--end;
+			v:SetDirectionVector(TryGetDir(v)); -- 
 			v.lastPos = v:GetPos();
 		end;
-		if(SIN_LOG_VERBOSITY and SIN_LOG_VERBOSITY>3)then
-			if(#allAIEntities==0)then
-				printf("$9[$4SiN$9] AISystem: no entities found to relocate directions ...")
-			elseif(#allAIEntities>0 and updated==0)then
-				printf("$9[$4SiN$9] AISystem: WARNING: all AI entities are missing lastHitTarget; skipping relocate")
-			elseif(updated~=#allAIEntities)then
-				printf("$9[$4SiN$9] AISystem: WARNING: " .. (#allAIEntities-updated).." entities are missing lastHitTarget; skipping relocate")
-			else
-				printf("$9[$4SiN$9] AISystem: relocated " .. updated .. " AIEntities")
-			end;
-		end;
-		else
-			-- dont print error like in old function -> else OVERFLOW :D
-		end;
+	else
+		
 	end;
 end
-
-
-        
-        
-        
-
-
-
-				
+			
 function SetAILogVerbosity(number)
 	if(not number)then
 		printf("    $3sin_aiLogVerbosity = $6"..SIN_LOG_VERBOSITY)
@@ -167,18 +121,6 @@ function SetAILogVerbosity(number)
 		SIN_LOG_VERBOSITY=0;
 		printf("    $3sin_aiLogVerbosity = $6"..SIN_LOG_VERBOSITY)
 	end;
-end;
-function SetAIUpdateRate(number)
-	if(not number)then
-		printf("$9[$4SiN$9] AISystem: update delay is " ..SIN_AI_UPDATE_DELAY );
-		return true;
-	end;
-	SIN_AI_UPDATE_DELAY = tonumber(number);
-	if(SIN_AI_UPDATE_DELAY<1)then
-		SIN_AI_UPDATE_DELAY=1;
-	end;
-	printf("$9[$4SiN$9] AISystem: new update delay is " ..SIN_AI_UPDATE_DELAY );
-	return true;
 end;
 function ToggleAIUpdate()
 	if not UPDATE_AI_ENTITIES then
@@ -191,7 +133,7 @@ function ToggleAIUpdate()
 	end;
 end;
 System.AddCCommand("sin_aiLogVerbosity", "SetAILogVerbosity(%%)", "Sets the new SiN-AISystem logging verbosity");
-System.AddCCommand("sin_aiUpdateDelay", "SetAIUpdateRate(%%)", "Sets the new SiN-AISystem updating Delay");
+--System.AddCCommand("sin_aiUpdateDelay", "SetAIUpdateRate(%%)", "Sets the new SiN-AISystem updating Delay");
 System.AddCCommand("sin_aiUpdateSystem", "ToggleAIUpdate()", "if true, AI Entities will be updated and relocated to their correct position");
 
 System.AddCCommand("sin_update", "DownloadLatest()", "re-downloads the SiN-AIFiles");
