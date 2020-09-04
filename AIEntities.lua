ad031aaa0afbf8f8f81f9c34f20144100a6748e5
FinchMaster101 <68505550+FinchMaster101@users.noreply.github.com>
2020-09-05 01:45:09 +0200

    Update AIEntities.lua

--- AIEntities.lua
+++ AIEntities.lua
@@ -4,7 +4,28 @@ if(not Scout)then Script.ReloadScript("Scripts/Entities/AI/Aliens/Scout.lua") en
 if(not Observer)then Script.ReloadScript("Scripts/Entities/AI/Aliens/Observer.lua") end;
 if(not Trooper)then Script.ReloadScript("Scripts/Entities/AI/Aliens/Trooper.lua") end;
 
+if(not OLD)then OLD = {}; end; -- in here all old functions are stored so patching will be easier.
 
 -- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= :: HUNTER UPDATES :: =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 -- [WIP] Coming soon
+
+
+-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= ::  SCOUT UPDATES  :: =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+if(not OLD.Scout_OldCLHit)then OLD.Scout_OldCLHit = Scout.Client:OnHit; end;
+
+function Scout_x.Client:OnHit(hit, remote)
+
+  local success, error = pcall(function() OLD.Scout_CLHit(hit, remote); end);
+  if(not success)then
+     if(SIN_LOG_VERBOSITY>2)then
+       printf("$9[$4SiN$9] Warning: failed to execute OldScoutHit " .. tostring(error));
+     end;
+     return false;
+  end;
+  
+  -- used for "Fix MOAC"-test
+  self.lastHitDirection = hit.dir;
+  
+end
