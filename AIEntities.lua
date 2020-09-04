b71317d4565cdb5aba26929c65ab89d0f1c9a3aa
FinchMaster101 <68505550+FinchMaster101@users.noreply.github.com>
2020-09-04 04:34:45 +0200

    Create AIEntities.lua

---
+++ AIEntities.lua
@@ -0,0 +1,10 @@
+if(not Hunter)then Script.ReloadScript("Scripts/Entities/AI/Aliens/Hunter.lua") end;
+if(not Alien)then Script.ReloadScript("Scripts/Entities/AI/Aliens/Alien.lua") end;
+if(not Scout)then Script.ReloadScript("Scripts/Entities/AI/Aliens/Scout.lua") end;
+if(not Observer)then Script.ReloadScript("Scripts/Entities/AI/Aliens/Observer.lua") end;
+if(not Trooper)then Script.ReloadScript("Scripts/Entities/AI/Aliens/Trooper.lua") end;
+
+
+-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= :: HUNTER UPDATES :: =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+-- [WIP] Coming soon
