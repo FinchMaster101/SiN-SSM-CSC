e9d8956731244b5b38b6fd79fd78b35c3a8c57c1
FinchMaster101 <68505550+FinchMaster101@users.noreply.github.com>
2020-09-07 21:40:23 +0200

    Update AIEntities.lua

--- AIEntities.lua
+++ AIEntities.lua
@@ -63,7 +63,7 @@ end
 -- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= ::  PLAYER UPDATES  :: =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 if(not Player)then Script.ReloadScript("Scripts/Entities/Actor/Player.lua"); end;
-function Player:OnAction(action, activation, value)
+function g_localActor:OnAction(action, activation, value)
 	-- gamerules needs to get all player actions all times
 	if (g_gameRules and g_gameRules.Client.OnActorAction) then
 		if (not g_gameRules.Client.OnActorAction(g_gameRules, self, action, activation, value)) then
@@ -110,7 +110,7 @@ function Player:OnAction(action, activation, value)
 end;
 
 
-function Player:SetPlMode()
+function g_localActor:SetPlMode()
 	if(self.plMode or self.plMode == 1)then
 		self.plMode = 0;
 	else
@@ -120,7 +120,7 @@ printf("PLMODE "..self.plMode)
 end;
 
 --if(not OLD.Player_ClUpdate)then OLD.Player_CLUpdate = g_localActor.Client.OnUpdate; end;
-function Player.Client:OnUpdateNew(frameTime)
+function g_localActor.Client:OnUpdateNew(frameTime)
 	if(self.plMode~=nil)then
 		local vehicleId = self.actor:GetLinkedVehicleId();
 		if(vehicleId)then
