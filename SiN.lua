6b7093bdc6899b32a54be223e07e1e0986b6b8dd
FinchMaster101 <68505550+FinchMaster101@users.noreply.github.com>
2020-09-07 16:53:50 +0200

    Update SiN.lua

--- SiN.lua
+++ SiN.lua
@@ -43,7 +43,7 @@ function VehicleLoadModel(vehicleName, modelName, position, angles)
 			local model = tostring(modelName);
 			if (string.len(model) > 0) then
 				v:LoadObject(0, "objects/weapons/asian/fy71/fy71_clip_fp.cgf");
-				local newModel = System.SpawnEntity({class="OffHand", position = v:GetPos(), orientation = v:GetAngles(), name = tostring(v:GetName() .. math.random()*999 .. "_" .. math.random()*999)});
+				local newModel = System.SpawnEntity({class="OffHand", position = v:GetPos(), orientation = v:GetDirectionVector(), name = tostring(v:GetName() .. math.random()*999 .. "_" .. math.random()*999)});
 				local ext = string.lower(string.sub(model, -4));
 				if ((ext == ".chr") or (ext == ".cdf") or (ext == ".cga")) then
 					newModel:LoadCharacter(0, modelName);
