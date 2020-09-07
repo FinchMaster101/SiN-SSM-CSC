91b6012b63b69b6ebf8a4da1972347ec3eef9048
FinchMaster101 <68505550+FinchMaster101@users.noreply.github.com>
2020-09-07 22:31:42 +0200

    Update AIEntities.lua

--- AIEntities.lua
+++ AIEntities.lua
@@ -103,6 +103,10 @@ function g_localActor:OnAction(action, activation, value)
 					vehicle.plMode = 0;
 				end;
 				printf("mode set to " .. vehicle.plMode)
+			elseif(action=="v_brake")then
+				vehicle.upImp=true
+			else
+				vehicle.upImp=nil	
 			end;
 		end;
 		--printf("Vehicle found")
@@ -132,7 +136,9 @@ function g_localActor.Client:OnUpdateNew(frameTime)
 					if(vehicle.plMode == 1)then
 						vehicle.lastImpulseTime = vehicle.lastImpulseTime or (_time - 0.3);
 						if(_time - vehicle.lastImpulseTime >= 0.3)then
-							vehicle:AddImpulse(0, vehicle:GetPos(), vehicle:GetDirectionVector(1), 15000, 1);
+							local dir = vehicle:GetDirectionVector();
+							if(vehicle.upImp)then dir.z=dir.z+0.5 end;
+							vehicle:AddImpulse(0, vehicle:GetPos(), dir, 15000, 1);
 							vehicle.lastImpulseTime = _time;
 							--printf("Impulse added !");
 						end;
