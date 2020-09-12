fb293952d667c95068ad0ad8782b391fb5b579c9
FinchMaster101 <68505550+FinchMaster101@users.noreply.github.com>
2020-09-13 01:33:47 +0200

    Update AIEntities.lua

--- AIEntities.lua
+++ AIEntities.lua
@@ -1,6 +1,6 @@
 System.Log("$9[$4SiN$9] Installing Entities patch ..") 
 
-FILE_VERSION = "2.5.3";
+FILE_VERSION = "2.5.4";
 
 if(not Hunter)then Script.ReloadScript("Scripts/Entities/AI/Aliens/Hunter.lua") end;
 if(not Alien)then Script.ReloadScript("Scripts/Entities/AI/Aliens/Alien.lua") end;
@@ -26,6 +26,83 @@ function Tornado:OnResetNew()
 end;
 
 for i, tornado in pairs(System.GetEntitiesByClass("Tornado") or {}) do tornado:OnResetNew(); end;
+
+
+
+GUI = {
+	Properties = {
+		objModel 					= "objects/library/storage/barrels/rusty_metal_barrel_d.cgf",
+		bRigidBody					= 1,
+		bResting 					= 1,
+		bUsable						= nil,
+		bPhysicalized				= 1,
+		fMass 						= 10,
+		GUIMaterial					= "test_hard",
+		GUIUsageDistance			= 1.5,
+		GUIUsageTolerance			= 0.75,
+		GUIWidth					= 512,
+		GUIHeight					= 512,
+		GUIDefaultScreen			= "test_hard",
+		GUIMouseCursor				= "test_hard",
+		GUIPreUpdate				= 1,
+		GUIMouseCursorSize			= 18,
+		GUIHasFocus					= 0,
+		color_GUIBackgroundColor 	= {0,0,0},
+		fileGUIScript				= "test_hard",
+	},
+	Client = {},
+	Server = {},
+		
+	---------------------------
+	--		OnSpawn
+	---------------------------
+	OnSpawn = function(self) 
+		self:OnReset()
+	end,
+	---------------------------
+	--		OnReset
+	---------------------------
+	OnReset = function(self)
+		self.Properties.bUsable = nil;
+		self:SetUpdatePolicy(ENTITY_UPDATE_VISIBLE);
+		local model=self.Properties.objModel;
+		local t=self:GetName():sub(-4);
+		if(t==".cga" or t==".cgf")then 
+			model=self:GetName();
+		end 
+		self:LoadObject(0, model);
+		self:DrawSlot(0, 1);
+		if (tonumber(self.Properties.bPhysicalized) ~= 0) then
+			local physParam = {
+				mass = self.Properties.fMass; -- * 400,
+			};
+			self:Physicalize(0, PE_RIGID, physParam);
+			if (tonumber(self.Properties.bResting) ~= 0) then
+				self:AwakePhysics(0);
+			else
+				self:AwakePhysics(1);
+			end
+		end
+		if(CryActon.IsServer())then
+			CryAction.CreateGameObjectForEntity(self.id);
+			CryAction.BindGameObjectToNetwork(self.id);
+		end;
+	end,
+	
+	IsUsable = function(self, user)	  
+		System.Log("GUI--> isUsable");
+		return 2;
+	end,
+
+};
+
+MakePickable(GUI);
+
+for i,v in ipairs(System.GetEntitiesByClass("GUI")or{})do
+	v:OnReset();
+end;
+
+
 
 if(not OLD)then OLD = {}; end; -- in here all old functions are stored so patching will be easier. 
 
