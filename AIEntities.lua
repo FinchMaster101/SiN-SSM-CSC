e9002b4ea20fc7605d3c08473376c119afc8e92b
FinchMaster101 <68505550+FinchMaster101@users.noreply.github.com>
2020-09-15 04:29:55 +0200

    Update AIEntities.lua

--- AIEntities.lua
+++ AIEntities.lua
@@ -1,6 +1,6 @@
 System.Log("$9[$4SiN$9] Installing Entities patch ..") 
 
-FILE_VERSION = "2.6.2";
+FILE_VERSION = "2.6.3";
 
 if(not Hunter)then Script.ReloadScript("Scripts/Entities/AI/Aliens/Hunter.lua") end;
 if(not Alien)then Script.ReloadScript("Scripts/Entities/AI/Aliens/Alien.lua") end;
