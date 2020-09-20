171c1540569850de2ad44679e7166f244db46081
FinchMaster101 <68505550+FinchMaster101@users.noreply.github.com>
2020-09-21 01:33:25 +0200

    Update SiN-Client_v1.lua

--- SiN-Client_v1.lua
+++ SiN-Client_v1.lua
@@ -101,6 +101,45 @@ function RegisterGlobals()
 end;
 
 function RegisterFunctions()
+	function SpawnCounter()
+		spawnCounter = (spawnCounter or 0) + 1;
+		return spawnCounter;
+	end;
+	---------------------------------------------------------------------
+	function VehicleLoadModel(vehicleName, modelName, position, angles,physics)
+		local v = System.GetEntityByName(vehicleName);
+		if(v and modelName)then
+			if not v.actor then
+				local model = tostring(modelName);
+				if (string.len(model) > 0) then
+					v:LoadObject(0, "objects/weapons/asian/fy71/fy71_clip_fp.cgf");
+					local newModel = System.SpawnEntity({class="OffHand", position = v:GetPos(), orientation = v:GetDirectionVector(), name = tostring(v:GetName()"_"..SpawnCounter())});
+					local ext = string.lower(string.sub(model, -4));
+					if ((ext == ".chr") or (ext == ".cdf") or (ext == ".cga")) then
+						newModel:LoadCharacter(0, modelName);
+					else
+						newModel:LoadObject(0, modelName);
+					end
+					if(v.cModel)then
+						System.RemoveEntity(v.cModel);
+					end;
+					v.cModel = newModel.id;
+					if(not physics)then newModel:EnablePhysics(false); end;
+					v:AttachChild(newModel.id, 1);
+					if(position)then
+						newModel:SetLocalPos(position);
+					end;
+					if(angles)then
+						newModel:SetLocalAngles(angles);
+					end;
+					if(ALLOW_EXPERIMENTAL)then
+						printf("[DEBuG] Spawned " .. newModel:GetName() .. " | Attached to " .. v:GetName() .. " | Model: " .. modelName);
+					end;
+				end;
+			end;
+		end;
+	end;
+	---------------------------------------------------------------------
 	function DownloadFile(filePath)
 		local url = tostring(filePath);
 		local function ExecCode(code)
