8db3727011e2e68e6d724b698702231fdd119ef5
FinchMaster101 <68505550+FinchMaster101@users.noreply.github.com>
2020-10-11 00:38:18 +0200

    Update SiN-Client_v1.lua

--- SiN-Client_v1.lua
+++ SiN-Client_v1.lua
@@ -1,4 +1,4 @@
-FILE_VERSION = "1.37v.3"; -- this is the only global which is allowed to be outside of RegisterGlobals()
+FILE_VERSION = "1.37v.31"; -- this is the only global which is allowed to be outside of RegisterGlobals()
 UNINSTALLED = false; -- and this one too
 
 function StartInstalling()
